ac3e58d1c956d1c1b9e53cbe9be5fb46dda4153e
Tony <tawn33y@gmail.com>
2024-05-15 19:00:19 -0400

    Update conv & op_conv to be fully working

--- src/layers/conv.cuh
+++ src/layers/conv.cuh
@@ -1,72 +1,96 @@
 #pragma once
-#include "ops/op_elemwise.cuh"
+#include "modules/param.cuh"
+#include "ops/op_conv.cuh"
 #include "ops/op_reduction.cuh"
-#include "ops/op_mm.cuh"
-#include "modules/convParams.cuh"
-#include "ops/op_padding.cuh"
 
 template<typename T>
 class ConvLayer {
+private:
+    int in_channels, out_channels, kernel_size;
+    int stride, padding;
+
+    Parameter<T> w;
+    Parameter<T> b;
 
 public:
-   string layerid;
-   int in_height;
-   int in_width;
-   int in_depth;
-   int out_height;
-   int out_width;
-   int w_height;
-   int w_width;
-   int num_filters;
-   int stride;
-   int padding;
-   ConvParameter<T> params;
+    ConvLayer(int in_channels_, int out_channels_, int kernel_size_, int stride_=1, int padding_=0, bool gpu=true)
+        : in_channels(in_channels_), out_channels(out_channels_), kernel_size(kernel_size_),
+          stride(stride_), padding(padding_)
+    {
+        w = Parameter<T>{out_channels, in_channels, kernel_size, kernel_size, gpu};
+        b = Parameter<T>{1, out_channels, 1, 1, gpu};
+    }
 
-   ConvLayer(string layer_id_, int in_height_, int in_width_, int in_depth_, int out_height_, int out_width_, int w_height_, int w_width_, int num_filters_, bool gpu, int stride_ = 1, int padding_ = 0)
-       : layerid(layer_id_), in_height(in_height_), in_width(in_width_), in_depth(in_depth_), out_height(out_height_), out_width(out_width_), w_height(w_height_), w_width(w_width_), num_filters(num_filters_), stride(stride_), padding(padding_) {
-       params = ConvParameter<T>{w_height, w_width, in_depth, num_filters, gpu};
-   }
+    ConvLayer() {}
 
-   ConvLayer() {}
+    ConvLayer(ConvLayer&& other)
+        : in_channels(other.in_channels), out_channels(other.out_channels), kernel_size(other.kernel_size),
+          stride(other.stride), padding(other.padding), w(std::move(other.w)), b(std::move(other.b)) {}
 
-   ConvLayer(ConvLayer&& other)
-       : in_height(other.in_height), in_width(other.in_width), in_depth(other.in_depth), out_height(other.out_height), out_width(other.out_width), w_height(other.w_height), num_filters(other.num_filters) {}
+    std::vector<Parameter<T>*> parameters() {
+        std::vector<Parameter<T>*> v;
+        v.push_back(&w);
+        v.push_back(&b);
+        return v;
+    }
 
-   void init_uniform() {
-       float max = 1.0f / std::sqrt(in_height + in_width + in_depth);
+    void init_uniform() {
+        float max = 1.0f / std::sqrt(in_channels * kernel_size * kernel_size);
+        op_uniform_init(w.t, -max, max);
+        op_uniform_init(b.t, -max, max);
+    }
 
-       for (int i = 0; i < num_filters; i++) {
-           for (int j = 0; j < in_depth; j++) {
-               op_uniform_init(params.weights[i].values[j], -max, max);
-               op_uniform_init(params.d_weights[i].values[j], -max, max);
-           }
-       }
-   }
+    void forward(const Tensor<T>& x, Tensor<T>& y) {
+        int batch_size = x.h;
+        int input_height = x.d1;
+        int input_width = x.d2;
+        int output_height = (input_height + 2 * padding - kernel_size) / stride + 1;
+        int output_width = (input_width + 2 * padding - kernel_size) / stride + 1;
 
-   void forward(const Tensor3D<float>& in, Tensor3D<float>& out) {
-       assert(in.d == params.weights[0].d && num_filters == out.d);
+        Tensor<T> x_padded = x.pad(padding);
+        y = Tensor<T>{batch_size, out_channels, output_height, output_width, true};
 
-       Tensor3D<float> in_device = in.toDevice();
-       Tensor3D<float> padded_tensor = Tensor3D<float>{in.h + 2 * padding, in.w + 2 * padding, in.d, true};
-       op_padding(in, padded_tensor, padding);
-       for (int i = 0; i < num_filters; i++) {
-           Tensor3D<float> w_t_device = params.weights[i].toDevice();
-           op_conv(padded_tensor, w_t_device, out.values[i], stride, padding);
-           op_relu(out.values[i], out.values[i]);
-       }
-   }
+        for (int b = 0; b < batch_size; ++b) {
+            for (int oc = 0; oc < out_channels; ++oc) {
+                for (int oh = 0; oh < output_height; ++oh) {
+                    for (int ow = 0; ow < output_width; ++ow) {
+                        Tensor<T> x_slice = x_padded.slice(b, b + 1, 0, in_channels, oh * stride, oh * stride + kernel_size, ow * stride, ow * stride + kernel_size);
+                        Tensor<T> w_slice = w.t.slice(oc, oc + 1, 0, in_channels, 0, kernel_size, 0, kernel_size);
 
-   void backward(const Tensor3D<float>& x, const Tensor3D<float>& dy, Tensor3D<float>& dx) {
-       Tensor3D<float> x_padded = Tensor3D<float>{x.h + 2 * padding, x.w + 2 * padding, x.d, true};
-       op_padding(x, x_padded, padding);
+                        op_gemm(x_slice, w_slice, y.slice(b, b + 1, oc, oc + 1, oh, oh + 1, ow, ow + 1));
+                    }
+                }
+                op_add(y.slice(b, b + 1, oc, oc + 1, 0, output_height, 0, output_width), b.t.slice(0, 1, oc, oc + 1, 0, 1, 0, 1), y.slice(b, b + 1, oc, oc + 1, 0, output_height, 0, output_width));
+            }
+        }
+    }
 
-       for (int i = 0; i < num_filters; i++) {
-           Tensor3D<float> dy_i = dy.values[i];
-           Tensor3D<float> dx_i = dx.values[i];
-           Tensor3D<float> w_t = params.weights[i];
+    void backward(const Tensor<T>& x, const Tensor<T>& dy, Tensor<T>& dx, Tensor<T>& dw, Tensor<T>& db) {
+        int batch_size = x.h;
+        int input_height = x.d1;
+        int input_width = x.d2;
+        int output_height = dy.d1;
+        int output_width = dy.d2;
 
-           op_conv_back_data(dy_i, w_t, x_padded, dx_i, stride, padding);
-           op_conv_back_weights(x_padded, dy_i, w_t, params.d_weights[i], stride, padding);
-       }
-   }
+        Tensor<T> x_padded = x.pad(padding);
+        dx = Tensor<T>{batch_size, in_channels, input_height, input_width, true};
+        dw = Tensor<T>{out_channels, in_channels, kernel_size, kernel_size, true};
+        db = Tensor<T>{1, out_channels, 1, 1, true};
+
+        for (int b = 0; b < batch_size; ++b) {
+            for (int oc = 0; oc < out_channels; ++oc) {
+                op_sum(dy.slice(b, b + 1, oc, oc + 1, 0, output_height, 0, output_width), db.slice(0, 1, oc, oc + 1, 0, 1, 0, 1));
+
+                for (int oh = 0; oh < output_height; ++oh) {
+                    for (int ow = 0; ow < output_width; ++ow) {
+                        Tensor<T> x_slice = x_padded.slice(b, b + 1, 0, in_channels, oh * stride, oh * stride + kernel_size, ow * stride, ow * stride + kernel_size);
+                        Tensor<T> dy_slice = dy.slice(b, b + 1, oc, oc + 1, oh, oh + 1, ow, ow + 1);
+
+                        op_gemm(dy_slice, x_slice.transpose(), dw.slice(oc, oc + 1, 0, in_channels, 0, kernel_size, 0, kernel_size), true, false);
+                        op_gemm(dy_slice, w.t.slice(oc, oc + 1, 0, in_channels, 0, kernel_size, 0, kernel_size), dx.slice(b, b + 1, 0, in_channels, oh * stride, oh * stride + kernel_size, ow * stride, ow * stride + kernel_size), false, true);
+                    }
+                }
+            }
+        }
+    }
 };
