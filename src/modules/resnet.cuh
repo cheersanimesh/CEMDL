bace6bdda335ff63cf4504f3cf928d65771c24a5
Ayman Charafeddine <ac11056@b-3-14.c.hpc-slurm-9c75.internal>
2024-05-15 20:55:14 -0400

    updated to resnet18

--- src/modules/resnet.cuh
+++ src/modules/resnet.cuh
@@ -77,34 +77,66 @@ public:
 template <typename T>
 class ResNet {
 public:
+    ConvLayer<T> initial_conv;
     std::vector<ResNetBlock<T>> layers;
     FlattenLayer<T> flatten;
+    Tensor<T> fc_weights;
+    Tensor<T> fc_bias;
     int num_classes;
 
     ResNet(int num_classes_, bool gpu) : num_classes(num_classes_) {
+        initial_conv = ConvLayer<T>("initial_conv", 1, 64, 7, 7, 2, 3, gpu);
+        layers.push_back(ResNetBlock<T>(64, 64, 1, gpu));
         layers.push_back(ResNetBlock<T>(64, 64, 1, gpu));
         layers.push_back(ResNetBlock<T>(64, 128, 2, gpu));
+        layers.push_back(ResNetBlock<T>(128, 128, 1, gpu));
         layers.push_back(ResNetBlock<T>(128, 256, 2, gpu));
+        layers.push_back(ResNetBlock<T>(256, 256, 1, gpu));
         layers.push_back(ResNetBlock<T>(256, 512, 2, gpu));
+        layers.push_back(ResNetBlock<T>(512, 512, 1, gpu));
         flatten = FlattenLayer<T>();
+        fc_weights = Tensor<T>(512, num_classes_, gpu);
+        fc_bias = Tensor<T>(1, num_classes_, gpu);
+        op_uniform_init(fc_weights);
+        op_const_init(fc_bias, 0.0);
     }
 
     void forward(const Tensor3D<T>& x, Tensor<T>& out) {
         Tensor3D<T> current = x;
+        initial_conv.forward(current, current);
         for (auto& layer : layers) {
             layer.forward(current, current);
         }
         flatten.forward(current, out);
+        Tensor<T> logits{out.h, fc_weights.w, out.on_device};
+        op_mm(out, fc_weights, logits);
+        op_add(logits, fc_bias, out);
     }
 
     void backward(const Tensor<T>& grad_output, const Tensor3D<T>& x, Tensor3D<T>& grad_input) {
         Tensor<T> grad_flatten;
-        flatten.backward(grad_output, grad_flatten);
+        Tensor<T> grad_fc_weights{fc_weights.h, fc_weights.w, fc_weights.on_device};
+        Tensor<T> grad_fc_bias{fc_bias.h, fc_bias.w, fc_bias.on_device};
+
+        // Gradient with respect to fully connected layer weights and bias
+        op_mm(grad_output.transpose(), flatten.y.transpose(), grad_fc_weights);
+        op_sum(grad_output, grad_fc_bias);
+
+        // Gradient with respect to the output of the flatten layer
+        op_mm(grad_output, fc_weights.transpose(), grad_flatten);
+
         Tensor3D<T> current_grad = grad_flatten.toDevice();
         for (auto it = layers.rbegin(); it != layers.rend(); ++it) {
             it->backward(current_grad, x, current_grad);
         }
         grad_input = current_grad;
+    }
+
+    std::vector<Tensor<T>> parameters() {
+        std::vector<Tensor<T>> params;
+        params.push_back(fc_weights);
+        params.push_back(fc_bias);
+        return params;
     }
 };
 
